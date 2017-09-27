ce33c72c6a04d644b110cdae1ae2146144b744b0
James Von Holle <jrvonhollejr@alaska.edu>
2017-09-27 17:00:30 +0000

    adding those good good comments

--- hw2/build.cpp
+++ hw2/build.cpp
@@ -4,37 +4,35 @@
 // 29 Sept 2017
 
 #include "build.h"
-#include <utility> // for std::pair and std::make_pair
-#include <cstddef>
 
+// see header for parameter detail
 int build(int w, int e, const std::vector<Bridge> & bridges) {
-    auto max_tol = 0;
-    auto cur_tol = 0;
+    auto max_tol = 0;  // the maximum toll
+    auto cur_tol = 0;  // the toll for the current set of bridges
     
-    auto cur_set = bridges;
-    cur_set.clear();
+    auto cur_set = bridges; // the current set of bridges
     bool cross = false;
     
     for(auto i : bridges) {
-        cur_set.clear();
-        cur_set.push_back(i);
-        cur_tol = i[2];
+        cur_set.clear();       // clear working set of bridges
+        cur_set.push_back(i);  // add current bridge from list to working set
+        cur_tol = i[2];        // set current toll to toll of the new first bridge
         for(auto j : bridges) {
             if(i == j)
                 continue;
                 
             cross = false;
-            for(auto k : cur_set) 
+            for(auto k : cur_set)  // check if new bridge crosses any of the old bridges
                 if( (j[0] <= k[0] && j[1] >= k[1]) || (j[0] >= k[0] && j[1] <= k[1]) )
                     cross = true;
                     
             if(!cross) {
-                cur_set.push_back(j);
+                cur_set.push_back(j);  // if it doesn't cross, add it to the list and update the toll
                 cur_tol += j[2];
             }
         }
         if(max_tol < cur_tol)
-            max_tol = cur_tol;
+            max_tol = cur_tol;  // update the max tol
     }
     
     return max_tol; 
--- hw2/build.h
+++ hw2/build.h
@@ -11,6 +11,15 @@
 
 using Bridge = std::vector<int>;
 
+// build
+//  takes two ints, w & e, which denote the number of "cities" on either side
+//  takes a vector of Bridge (vector<int>), bridges, with all the possible bridges
+//     **PRE-CONDITION:
+//                     A bridge must be exactly 3 ints
+//                         The first denotes the left city
+//                         The second denotes the right city
+//                         The third denotes the toll for that bridge
+//  returns the maximum possible toll with the given bridges
 int build(int w, int e, const std::vector<Bridge> & bridges);
 
 #endif
