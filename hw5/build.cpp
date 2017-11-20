14a39904bd3443b53808952c2a018eadd17bd4cb
jvonholle <jrvonhollejr@alaska.edu>
2017-11-20 05:19:34 -0900

    cleaned up hw5

--- hw5/build.cpp
+++ hw5/build.cpp
@@ -7,22 +7,11 @@
 #include <algorithm>
 
 #include <iostream>
+#include <chrono>
 
 bool crosses(Bridge left, Bridge right) { return ((left[0] <= right[0] && left[1] >= right[1]) || (left[0] >= right[0] && left[1] <= right[1])); }
 
-void printMatrix(std::vector<std::vector<int> > mat) {
-    for(size_t i = 0; i < mat.size(); ++ i) {
-        for(size_t j = 0; j < mat[i].size(); ++j) {
-            if(mat[i][j] < 10)
-                std::cout << 0;
-            std::cout << mat[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << "--------------------------------------------" << std::endl;
-}
-
-inline std::vector< std::vector<int> > makeBridgeMatrix(const std::vector<Bridge> & bridges, const int & e) {
+static inline std::vector< std::vector<int> > makeBridgeMatrix(const std::vector<Bridge> & bridges, const int & e) {
     std::vector< std::vector<int> > bridgeMat;
     std::vector<int> temp(e, 0);
     
@@ -49,27 +38,14 @@ inline int handleTiny(const std::vector<Bridge> & bridges) {
 // details on parameters in header
 int build(int w, int e, const std::vector<Bridge> & bridges) { 
     if(e <= 2) return handleTiny(bridges);
-    
+
     auto bridgeMat = makeBridgeMatrix(bridges, e); 
-    int max = 0;
-    
-    for(int i = 0; i < e; ++i) {
-        for(int j = 0; j < e; ++j) {
-            if(i > 0 && j > 0)
-                bridgeMat[i][j] = std::max({(bridgeMat[i][j]+bridgeMat[i-1][j-1]), bridgeMat[i][j-1], bridgeMat[i-1][j]});
-            max = std::max(max, bridgeMat[i][j]);
-        }
-    }
-    
-    return max;
+
+    for(int i = 1; i < e; ++i)
+        for(int j = 1; j < e; ++j)
+            bridgeMat[i][j] = std::max({(bridgeMat[i][j]+bridgeMat[i-1][j-1]), bridgeMat[i][j-1], bridgeMat[i-1][j]});
+
+
+    return bridgeMat[w-1][e-1];
 }
 
-// Top Down
-// details on parameters in header
-// int build(int w, int e, const std::vector<Bridge> & bridges) { 
-//     if(e <= 2) return handleTiny(bridges);
-    
-    
-    
-//     return 0;
-// }
